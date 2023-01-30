0ac80dedfed28a5dc770ee07a59adf7b95d831f3
natemcoleman <natecolemancv@gmail.com>
2023-01-30 10:31:30 -0700

    Changed code to cross-frame flasher optimization

--- main.py
+++ main.py
@@ -1,168 +1,80 @@
 # Initial python script for optimization final project
 
-from math import sqrt, pi, floor, ceil
+from math import sqrt, pi, floor, ceil, acos
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize, brute
+from tqdm import tqdm
 
-sideLength = 1
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f"x:{self.x} y:{self.y}"
 
-def checkDistance(point1, point2):
-    return sqrt(((point2[0]-point1[0])**2)+((point2[1]-point1[1])**2))
+class line:
+    # points = [point(0,0), point(1,1)]
+    def __init__(self, firstInitialPointForLine, secondInitialPointForLine):
+        self.points = []
+        # self.points[0] = firstInitialPointForLine
+        # self.points[1] = secondInitialPointForLine
+        self.points.append(firstInitialPointForLine)
+        self.points.append(secondInitialPointForLine)
 
-def calculateAreaEfficiency(n, circleRadius):
-    return ((pi*(circleRadius**2))/(n*sideLength*sideLength))*100
-
-def findAreaEfficiencyFromGridOrder(squareGridOrder):
-
-    # # print("Area efficiency with ", n, " cells is ", calculateAreaEfficiency(n, circleRadius), "%")
-    # # print("Overall circle efficiency with ", n, " cells is ", calculateAreaEfficiency(n, circleRadius)/0.78539816339, "%")
-    #
-    # # if squareGridOrder>10:
-    # #     plotCurrentGrid(squareGridOrder)
-    # print("with order " ,  squareGridOrder, " p is ", p)
-    # narray.append(int(squareGridOrder))
-    # nefficiency.append(float(calculateAreaEfficiency(p, circleRadius)))
-
-    p = squareGridOrder ** 2
-    circleCenter = (sideLength * 0.5 * squareGridOrder, sideLength * 0.5 * squareGridOrder)
-    circleRadius = sideLength * 0.5 * squareGridOrder
-
-    for i in range(int(squareGridOrder)):
-        for j in range(int(squareGridOrder)):
-            checkPoint = (0, 0)
-            squareCenter = ((i * sideLength) + 0.5, (j * sideLength) + 0.5)
-
-            # find if square is within circle
-            if (squareCenter[0] < circleCenter[0]):
-                if (squareCenter[1] < circleCenter[1]):
-                    checkPoint = (squareCenter[0] + 0.5 * sideLength, squareCenter[1] + 0.5 * sideLength)
-                elif (squareCenter[1] == circleCenter[1]):
-                    checkPoint = (squareCenter[0] + 0.5 * sideLength, squareCenter[1])
-                else:
-                    checkPoint = (squareCenter[0] + 0.5 * sideLength, squareCenter[1] - 0.5 * sideLength)
-
-            elif (squareCenter[0] == circleCenter[0]):
-                if (squareCenter[1] < circleCenter[1]):
-                    checkPoint = (squareCenter[0], squareCenter[1] + 0.5 * sideLength)
-                elif (squareCenter[1] == circleCenter[1]):
-                    checkPoint = (squareCenter[0], squareCenter[1])
-                else:
-                    checkPoint = (squareCenter[0], squareCenter[1] - 0.5 * sideLength)
-
-            else:
-                if (squareCenter[1] < circleCenter[1]):
-
-                    checkPoint = (squareCenter[0] - 0.5 * sideLength, squareCenter[1] + 0.5 * sideLength)
-                elif (squareCenter[1] == circleCenter[1]):
-                    checkPoint = (squareCenter[0] - 0.5 * sideLength, squareCenter[1])
-                else:
-
-                    checkPoint = (squareCenter[0] - 0.5 * sideLength, squareCenter[1] - 0.5 * sideLength)
-
-            if checkDistance(checkPoint, circleCenter) > circleRadius:
-                p -= 1
-
-    narray.append(int(squareGridOrder))
-    nefficiency.append(float(calculateAreaEfficiency(p, circleRadius)))
-    # print("Area efficiency with ", p, " cells in a ", squareGridOrder, " x ", squareGridOrder, " grid with ",
-    #       (squareGridOrder ** 2) - p, " cells removed is ", calculateAreaEfficiency(p, circleRadius), "%")
-    # print("The circle radius is ", circleRadius)
-
-    return -calculateAreaEfficiency(p, circleRadius)
-
-def plotCurrentGrid(squareGridOrder):
-    n = squareGridOrder**2
-    plt.axes()
-    circleCenter = (sideLength*0.5*squareGridOrder, sideLength*0.5*squareGridOrder)
-    circleRadius = sideLength*0.5*squareGridOrder
-    squareValidColor = (199/255, 201/255, 199/255, 1)
-    squareValidBorderColor = (0/255, 46/255, 96/255, 1)
-    squareInvalidColor = (209/255, 65/255, 36/255, 1)
-    circleColor = (158/255, 42/255, 43/255, 0.5)
-
-    for i in range(int(squareGridOrder)):
-        for j in range(int(squareGridOrder)):
-            checkPoint = (0,0)
-            squareCenter = ((i*sideLength)+0.5, (j*sideLength)+0.5)
-
-            #find if square is within circle
-            if(squareCenter[0] < circleCenter[0]):
-                if(squareCenter[1] < circleCenter[1]):
-                    checkPoint = (squareCenter[0] + 0.5*sideLength, squareCenter[1] + 0.5*sideLength)
-                elif (squareCenter[1] == circleCenter[1]):
-                    checkPoint = (squareCenter[0] + 0.5*sideLength, squareCenter[1])
-                else:
-                    checkPoint = (squareCenter[0] + 0.5*sideLength, squareCenter[1] - 0.5*sideLength)
-
-            elif (squareCenter[0] == circleCenter[0]):
-                if (squareCenter[1] < circleCenter[1]):
-                    checkPoint = (squareCenter[0], squareCenter[1] + 0.5 * sideLength)
-                elif (squareCenter[1] == circleCenter[1]):
-                    checkPoint = (squareCenter[0], squareCenter[1])
-                else:
-                    checkPoint = (squareCenter[0], squareCenter[1] - 0.5 * sideLength)
-
-            else:
-                if(squareCenter[1] < circleCenter[1]):
-
-                    checkPoint = (squareCenter[0] - 0.5*sideLength, squareCenter[1] + 0.5*sideLength)
-                elif (squareCenter[1] == circleCenter[1]):
-                    checkPoint = (squareCenter[0] - 0.5 * sideLength, squareCenter[1])
-                else:
-
-                    checkPoint = (squareCenter[0] - 0.5*sideLength, squareCenter[1] - 0.5*sideLength)
-
-            if checkDistance(checkPoint, circleCenter) <= circleRadius:
-                rectangle = plt.Rectangle((i * sideLength, j * sideLength), sideLength, sideLength, fc=squareValidColor,ec=squareValidBorderColor)
-            else:
-                rectangle = plt.Rectangle((i * sideLength, j * sideLength), sideLength, sideLength, fc=squareInvalidColor,ec=squareValidBorderColor)
-                n -= 1
-            plt.gca().add_patch(rectangle)
-
-            # print("Distance between ", checkPoint, " and ", circleCenter, " is ", checkDistance(checkPoint, circleCenter))
-            # print("Circle radius is ", circleRadius)
-            # circle = plt.Circle(checkPoint, .1, color='green')
-            # circle = plt.Circle(squareCenter, .1, color='green')
-            # plt.gca().add_patch(circle)
-            plt.axis('scaled')
-    circle = plt.Circle(circleCenter, circleRadius, color=circleColor)
-    plt.gca().add_patch(circle)
-    plt.axis('scaled')
-    plt.axis('off')
-    print("Area efficiency with ", n, " cells in a ", squareGridOrder, " x " , squareGridOrder, " grid with ", (squareGridOrder**2) - n, " cells removed is ", calculateAreaEfficiency(n, circleRadius), "%")
-    print("The circle radius is ", circleRadius)
-    plt.show()
+    def __str__(self):
+        return f"({self.points[0].x},{self.points[0].y}), ({self.points[1].x},{self.points[1].y})"
 
 
-# def constraint1(squareGridOrder):
-#     return [squareGridOrder - 2]
-#
-# def constraint2(squareGridOrder):
-#     return [10 - squareGridOrder]
+def distance(a,b):
+    return sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
 
-# con1 = {'type': 'ineq', 'fun': constraint1}
-# con2 = {'type': 'ineq', 'fun': constraint2}
-# cons = [con1, con2]
-# cons = [con1]
-# intialGridOrderGuess = 4
+def is_between_points(a,b,c):
+    return distance(a,c) + distance(c,b) == distance(a,b)
+def is_on_line(line,point):
+    return distance(line.points[0],point) + distance(point,line.points[1]) == distance(line.points[0],line.points[1])
 
-maxCells = 10000
-minCells = 1
-functionRanges = (slice(floor(sqrt(minCells)), floor(sqrt(maxCells)), 1),)
-narray = []
-nefficiency = []
-# result = minimize(f, a0, constraints=cons, options = opt)
-# result = minimize(findAreaEfficiencyFromGridOrder, intialGridOrderGuess, constraints=cons)
-# result = minimize(findAreaEfficiencyFromGridOrder, intialGridOrderGuess)
-result = brute(findAreaEfficiencyFromGridOrder, functionRanges, finish = None)
+def constraint1(guessPoint):
+    trueStatements = [is_on_line(i, guessPoint) for i in polygonLines]
+    return any(trueStatements)-1
 
-# print(result)
+con1 = {'type': 'eq', 'fun': constraint1}
+cons = [con1]
 
-plt.plot(narray, nefficiency)
-plt.xlabel('Number of Cells on Each Side')
-plt.ylabel('Area Efficiency (%)')
-plt.show()
-# for k in range(3):
-#     plotCurrentGrid(k+6)
 
-plotCurrentGrid(result)
+point1 = point(0,0)
+point2 = point(0,1)
+point3 = point(1,1)
+point4 = point(1,0)
+
+line1 = line(point1, point2)
+line2 = line(point2, point3)
+line3 = line(point3, point4)
+line4 = line(point1, point4)
+
+polygonLines = [line1, line2, line3, line4]
+
+point5 = point(0.5, 0.5)
+point6 = point(0.5,0)
+
+# print("point 5 is between:", is_between_points(point1, point2,point5), ". This should be False.")
+# print("point 6 is between:", is_between_points(point1, point4,point6), ". This should be True.")
+# print("Point 1 is on line 1:", is_on_line(line1, point1), ". This should be True.")
+# print("Point 3 is on line 1:", is_on_line(line1, point3), ". This should be False.")
+# print("Point 5 is on line 4:", is_on_line(line4, point5), ". This should be False.")
+# print("Point 6 is on line 4:", is_on_line(line4, point6), ". This should be True.")
+
+# print("Point 1 is on any line", constraint1(point1), ". This should be True.")
+# print("Point 2 is on any line", constraint1(point2), ". This should be True.")
+# print("Point 3 is on any line", constraint1(point3), ". This should be True.")
+# print("Point 4 is on any line", constraint1(point4), ". This should be True.")
+# print("Point 5 is on any line", constraint1(point5), ". This should be False.")
+# print("Point 6 is on any line", constraint1(point6), ". This should be True.")
+
+# print(line1)
+# print(line2)
+# print(line3)
+# print(line4)
+# print(point1)
+# print(point2)
+# print(point3)
+# print(point4)
