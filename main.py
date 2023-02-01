fc4d69d649e47b7476529e13bee16642fb396da4
natemcoleman <natecolemancv@gmail.com>
2023-02-01 13:58:50 -0700

    added paths

--- main.py
+++ main.py
@@ -80,10 +80,17 @@ def GetMassOfAllLines(linesToGetMass):
     totalMass = 0
     for m in range(len(linesToGetMass)):
         totalMass += GetMassOfLine(linesToGetMass[m])
-        print("Mass of line ", m, ":", GetMassOfLine(linesToGetMass[m]))
+        # print("Mass of line ", m, ":", GetMassOfLine(linesToGetMass[m]))
 
     return totalMass
 
+
+def PrintMassOfAllLines(linesToGetMass):
+    totalMass = 0
+    for m in range(len(linesToGetMass)):
+        totalMass += GetMassOfLine(linesToGetMass[m])
+        print("Mass of line ", m, ":", GetMassOfLine(linesToGetMass[m]))
+    print("Total mass:", totalMass)
 
 class point:
     def __init__(self, x, y):
@@ -114,19 +121,37 @@ def is_on_any_line(guessPoint):
     return any(trueStatements)
 
 
-def plotShape(polygonLinesToPlot):
+def plotShape(linesToPlot,  numberOfPolygonLines):
     plt.axis('off')
-    xValuesToPlot = []
-    yValuesToPlot = []
-    for p in range(len(polygonLinesToPlot)):
-        plt.plot(polygonLinesToPlot[p].points[0].x, polygonLinesToPlot[p].points[0].y, 'r*')
-        plt.plot(polygonLinesToPlot[p].points[1].x, polygonLinesToPlot[p].points[1].y, 'r*')
-        xValuesToPlot.append(polygonLinesToPlot[p].points[0].x)
-        xValuesToPlot.append(polygonLinesToPlot[p].points[1].x)
-        yValuesToPlot.append(polygonLinesToPlot[p].points[0].y)
-        yValuesToPlot.append(polygonLinesToPlot[p].points[1].y)
+    xValuesToPlotShape = []
+    yValuesToPlotShape = []
+    xValuesToPlotPath = []
+    yValuesToPlotPath = []
+    for p in range(len(linesToPlot)):
+        if p < numberOfPolygonLines:
+            plt.plot(linesToPlot[p].points[0].x, linesToPlot[p].points[0].y, 'r*')
+            plt.plot(linesToPlot[p].points[1].x, linesToPlot[p].points[1].y, 'r*')
+            xValuesToPlotShape.append(linesToPlot[p].points[0].x)
+            xValuesToPlotShape.append(linesToPlot[p].points[1].x)
+            yValuesToPlotShape.append(linesToPlot[p].points[0].y)
+            yValuesToPlotShape.append(linesToPlot[p].points[1].y)
+        else:
+            plt.plot(linesToPlot[p].points[0].x, linesToPlot[p].points[0].y, 'go')
+            plt.plot(linesToPlot[p].points[1].x, linesToPlot[p].points[1].y, 'go')
+            xValuesToPlotPath.append(linesToPlot[p].points[0].x)
+            xValuesToPlotPath.append(linesToPlot[p].points[1].x)
+            yValuesToPlotPath.append(linesToPlot[p].points[0].y)
+            yValuesToPlotPath.append(linesToPlot[p].points[1].y)
 
-    plt.plot(xValuesToPlot, yValuesToPlot, 'b')
+    xValuesToPlotShape.pop()
+    yValuesToPlotShape.pop()
+
+    xValuesToPlotPath.pop()
+    yValuesToPlotPath.pop()
+
+    plt.plot(xValuesToPlotShape, yValuesToPlotShape, 'b--')
+    plt.plot(xValuesToPlotPath, yValuesToPlotPath, 'g-')
+
     ax = plt.gca()
     ax.set_aspect(1)
 
@@ -135,8 +160,34 @@ def plotShape(polygonLinesToPlot):
     plt.show()
 
 
-con1 = {'type': 'eq', 'fun': is_on_any_line}
-cons = [con1]
+def constraint1(point5Optimal):
+    return -(point5Optimal[0]-minDistance)
+def constraint2(point5Optimal):
+    return (point5Optimal[0]-minDistance)
+def constraint3(point5Optimal):
+    return -(point5Optimal[1]-minDistance)
+def constraint4(point5Optimal):
+    return (point5Optimal[1]-minDistance)
+
+con1 = {'type': 'ineq', 'fun': constraint1}
+con2 = {'type': 'ineq', 'fun': constraint2}
+con3 = {'type': 'ineq', 'fun': constraint3}
+con4 = {'type': 'ineq', 'fun': constraint4}
+cons = [con1, con2, con3, con4]
+
+
+# pointsInitialGuess = [(0, 0), (1, 0), (1, 1), (0, 1)]
+point5InitialGuess = [(0.25, 0.375)]
+
+def functionToMinimize(point5Optimal):
+    point5New = point(point5Optimal[0], point5Optimal[1])
+    line5 = line(point1, point5New)
+    line6 = line(point2, point5New)
+    line7 = line(point3, point5New)
+    line8 = line(point4, point5New)
+    pathLinesNew = [line5, line6, line7, line8]
+
+    return GetMassOfAllLines(pathLinesNew)
 
 
 point1 = point(0,0)
@@ -149,19 +200,40 @@ line2 = line(point2, point3)
 line3 = line(point3, point4)
 line4 = line(point1, point4)
 
+
 polygonLines = [line1, line2, line3, line4]
 
-point5 = point(0.5, 0.5)
-point6 = point(0.5,0)
+
+minDistance = 0.1
+opt = {'maxiter': 500}
+result = minimize(functionToMinimize, point5InitialGuess, constraints=cons, options=opt)
+print(result)
+
+point5 = point(result.x[0], result.x[1])
+
+line5 = line(point1, point5)
+line6 = line(point2, point5)
+line7 = line(point3, point5)
+line8 = line(point4, point5)
+
+pathLines = [line5, line6, line7, line8]
+
+plotLines = polygonLines.copy()
+plotLines.extend(pathLines)
+
+# print("Mass of lines:", GetMassOfAllLines(polygonLines))
+# print("Mass of path lines:", GetMassOfAllLines(pathLines))
+PrintMassOfAllLines(pathLines)
+# for l in range(len(plotLines)):
+#     print(plotLines[l])
+# plotShape(polygonLines)
+plotShape(plotLines, len(polygonLines))
 
 
-print("Mass of lines:", GetMassOfAllLines(polygonLines))
-
-plotShape(polygonLines)
 
 
 # print("point 5 is between:", is_between_points(point1, point2,point5), ". This should be False.")
-# print("point 6 is between:", is_between_points(point1, point4,point6), ". This should be True.")
+# print("point 1 is between:", is_between_points(point1, point4, point1), ". This should be True.")
 # print("Point 1 is on line 1:", is_on_line(line1, point1), ". This should be True.")
 # print("Point 3 is on line 1:", is_on_line(line1, point3), ". This should be False.")
 # print("Point 5 is on line 4:", is_on_line(line4, point5), ". This should be False.")
@@ -173,12 +245,3 @@ plotShape(polygonLines)
 # print("Point 4 is on any line", is_on_any_line(point4), ". This should be True.")
 # print("Point 5 is on any line", is_on_any_line(point5), ". This should be False.")
 # print("Point 6 is on any line", is_on_any_line(point6), ". This should be True.")
-
-# print(line1)
-# print(line2)
-# print(line3)
-# print(line4)
-# print(point1)
-# print(point2)
-# print(point3)
-# print(point4)
